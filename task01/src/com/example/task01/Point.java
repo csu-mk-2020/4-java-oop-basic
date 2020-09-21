68009b6fff71efe079261ee7e61b2461f8d27fe7
Kirito1899 <alex-kadykov@bk.ru>
2020-09-21 17:12:34 +0500

    Task01 [Кадыков]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -11,4 +11,27 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    Point() {
+    }
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int tmp = -this.x;
+        this.x = -this.y;
+        this.y = tmp;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
 }
