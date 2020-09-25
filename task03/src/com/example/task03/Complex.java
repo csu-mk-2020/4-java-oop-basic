7c0f802161b32a1edc060bedf6cfbca5c888d8e0
Arthur <parabellum357@hotmail.com>
2020-09-25 10:31:25 +0500

    Task03 [Саитгалин]

--- task03/src/com/example/task03/Complex.java
+++ task03/src/com/example/task03/Complex.java
@@ -1,30 +1,35 @@
 package com.example.task03;
 
+import java.util.Objects;
+
 public class Complex {
 
     double re;
     double im;
 
-    Complex(double re, double im){
+    Complex(double re, double im) {
         this.re = re;
         this.im = im;
     }
 
-    Complex add(Complex rhs){
+    Complex add(Complex rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
         return new Complex(this.re + rhs.re, this.im + rhs.im);
     }
 
-    Complex sub(Complex rhs){
+    Complex sub(Complex rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
         return new Complex(this.re - rhs.re, this.im - rhs.im);
     }
 
-    Complex mult(Complex rhs){
+    Complex mult(Complex rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
         return new Complex(
-                this.re*rhs.re - this.im*rhs.im,
-                this.im*rhs.re + this.re*rhs.im);
+                this.re * rhs.re - this.im * rhs.im,
+                this.im * rhs.re + this.re * rhs.im);
     }
 
-    public String toString(){
+    public String toString() {
         return String.format("%g %+gi", this.re, this.im);
     }
 }
