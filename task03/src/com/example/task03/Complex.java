eb0c349579c66019d620d1a65ece8079a7168163
Kirito1899 <alex-kadykov@bk.ru>
2020-09-25 14:05:24 +0500

    Task03 [Кадыков]

---
+++ task03/src/com/example/task03/Complex.java
@@ -0,0 +1,41 @@
+package com.example.task03;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+import org.omg.CORBA.IMP_LIMIT;
+
+public class Complex {
+    double Re;
+    double Im;
+
+    Complex() {
+    }
+
+    Complex(double r) {
+        Re = r;
+        Im = 0;
+    }
+
+    Complex(double Re, double Im) {
+        this.Re = Re;
+        this.Im = Im;
+    }
+
+    double abs() {
+        return Math.sqrt(Re * Re + Im * Im);
+    }
+
+    Complex sum(Complex com) {
+
+        return new Complex(this.Re + com.Re, this.Im + com.Im);
+    }
+
+    Complex mul(Complex com) {
+        return new Complex(this.Re * com.Re - this.Im * com.Im, this.Re * com.Im + this.Im * com.Re);
+    }
+
+    public String toString() {
+
+        return String.format("(%f %fi)", this.Re, this.Im);
+    }
+
+}
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,13 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        Complex a = new Complex(1, 2);
+        Complex b = new Complex(1, 1);
+        Complex c = b.sum(a);
+        Complex d = a.mul(b);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 }
