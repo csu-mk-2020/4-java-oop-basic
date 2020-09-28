0d9cf91c6d782f44360098acd986bde3edb66852
redrockstyle <ulowpie@gmail.com>
2020-09-28 21:16:02 +0500

    [Khazov] task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,7 +6,20 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
-
+    public Point(){}
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+    public String toString(){
+        return String.format("(%d,%d)",x,y);
+    }
+    double distance(Point point){
+        return Math.sqrt(Math.pow(x-point.x,2)+Math.pow(y-point.y,2));
+    }
+    void flip(){
+        x=-x-y-(y=-x);
+    }
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
