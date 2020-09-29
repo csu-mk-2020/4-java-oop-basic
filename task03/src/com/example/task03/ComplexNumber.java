561fc30316e8ed4efa3e6f886c625d1131c3a9d5
vadim_pleshkov <pleshkov_vadim@f-lab.tech>
2020-09-29 15:10:50 +0000

    Task 03 [Плешков]

---
+++ task03/src/com/example/task03/ComplexNumber.java
@@ -0,0 +1,54 @@
+package com.example.task03;
+
+import java.util.Objects;
+
+public class ComplexNumber {
+    private double real;
+    private double image;
+
+    ComplexNumber() {
+        this.real = 0;
+        this.image = 0;
+    }
+
+    ComplexNumber(double real, double image) {
+        this.real = real;
+        this.image = image;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImage() {
+        return image;
+    }
+
+    public void setReal(double real){
+        this.real = real;
+    }
+
+    public void setImage(double image){
+        this.image = image;
+    }
+
+    ComplexNumber add(ComplexNumber rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
+        return new ComplexNumber(
+                this.real + rhs.real,
+                this.image + rhs.image
+        );
+    }
+
+    ComplexNumber mult(ComplexNumber rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
+        return new ComplexNumber(
+                this.real * rhs.real - this.image * rhs.image,
+                this.image * rhs.real + this.real * rhs.image
+        );
+    }
+    
+    public String toString() {
+        return String.format("%f+%fi", this.real, this.image);
+    }
+}
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,6 +2,9 @@ package com.example.task03;
 
 public class Task03Main {
     public static void main(String[] args) {
-
+        ComplexNumber c1 = new ComplexNumber(13, 42);
+        ComplexNumber c2 = new ComplexNumber(-5, 21);
+        System.out.println(c1.add(c2).toString());
+        System.out.println(c1.mult(c2).toString());
     }
 }
