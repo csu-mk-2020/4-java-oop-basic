f72b378ab8f7798655a82bcf262dfb798689f9f6
Pavel Ryabinin <rpk9810@gmail.com>
2020-09-30 11:56:12 +0500

    Task01 [Рябинин]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -7,6 +7,36 @@ public class Point {
     int x;
     int y;
 
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int tmp = this.x;
+        this.x = this.y * -1;
+        this.y = tmp * -1;
+    }
+
+    public double distance(Point point) {
+        if (point == null) {
+            throw new IllegalArgumentException("Point must be not null");
+        }
+        double dx = Math.abs(this.x - point.x);
+        double dy = Math.abs(this.y - point.y);
+
+        return Math.hypot(dx, dy);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
