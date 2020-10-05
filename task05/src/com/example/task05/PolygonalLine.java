0a34003dd48c7ef1302853ae4e3727934f75e97f
redrockstyle <ulowpie@gmail.com>
2020-10-05 20:06:03 +0500

    [Khazov] task05

--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,9 +1,12 @@
 package com.example.task05;
 
+import java.util.ArrayList;
+
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    private final ArrayList<Point> points = new ArrayList<>();
 
     /**
      * Устанавливает точки ломаной линии
@@ -11,7 +14,11 @@ public class PolygonalLine {
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
     public void setPoints(Point[] points) {
-        // TODO: реализовать
+        if (points != null) {
+            for (Point p : points) {
+                this.points.add(new Point(p.getX(), p.getY()));
+            }
+        } else throw new IllegalArgumentException("аргумент пуст");
     }
 
     /**
@@ -20,7 +27,9 @@ public class PolygonalLine {
      * @param point точка, которую нужно добавить к ломаной
      */
     public void addPoint(Point point) {
-        // TODO: реализовать
+        if (point != null) {
+            points.add(new Point(point.getX(), point.getY()));
+        } else throw new IllegalArgumentException("аргумент пуст");
     }
 
     /**
@@ -30,7 +39,7 @@ public class PolygonalLine {
      * @param y координата по оси ординат
      */
     public void addPoint(double x, double y) {
-        // TODO: реализовать
+        points.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +48,11 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
-        // TODO: реализовать
-        throw new AssertionError();
+        double length = 0;
+        for (int i = 0; i < points.size() - 1; i++) {
+            length += points.get(i).getLength(points.get(i + 1));
+        }
+        return length;
     }
 
 }
--- task05/src/com/example/task05/Task05Main.java
+++ task05/src/com/example/task05/Task05Main.java
@@ -1,5 +1,7 @@
 package com.example.task05;
 
+import org.assertj.core.util.Arrays;
+
 public class Task05Main {
     public static void main(String[] args) {
 
