a1aa33019e81bfec682a6214bd36131d150b0a68
Aleksey <alex-kadykov@bk.ru>
2020-10-09 13:19:02 +0500

    Task03 [Кадыков] [исп]

---
+++ task03/src/com/example/task03/Complex.java
@@ -0,0 +1,30 @@
+package com.example.task03;
+
+import java.util.Objects;
+
+public class Complex {
+
+    final double re;
+    final double im;
+
+    Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
+    }
+
+    Complex sum(Complex rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
+        return new Complex(this.re + rhs.re, this.im + rhs.im);
+    }
+
+    Complex mult(Complex rhs) throws NullPointerException {
+        Objects.requireNonNull(rhs);
+        return new Complex(
+                this.re * rhs.re - this.im * rhs.im,
+                this.im * rhs.re + this.re * rhs.im);
+    }
+
+    public String toString() {
+        return String.format("%f %fi", this.re, this.im);
+    }
+}
