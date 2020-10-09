0be00a3d898f69e4c6bf7601ebb02aad19d0bdd3
Aleksey <alex-kadykov@bk.ru>
2020-10-09 13:15:30 +0500

    Task01 [Кадыков] [исп]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,4 +1,6 @@
 package com.example.task01;
+import java.util.Objects;
+import org.omg.PortableServer.POAPackage.ObjectNotActive;
 
 /**
  * Класс точки на плоскости
@@ -11,4 +13,27 @@ public class Point {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int tmp = -this.x;
+        this.x = -this.y;
+        this.y = tmp;
+    }
+
+    double distance(Point point)  {
+        if (point == null) {
+            throw new IllegalArgumentException("point не может быть null");
+        }
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
 }
