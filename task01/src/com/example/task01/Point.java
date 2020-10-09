682f0b87e54b632ff4adbac476be1e176ee327f4
redrockstyle <ulowpie@gmail.com>
2020-10-09 11:21:01 +0500

    [Khazov] task01

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,20 +6,26 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
-    public Point(){}
-    public Point(int x, int y){
+
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    public String toString(){
-        return String.format("(%d,%d)",x,y);
+
+    public String toString() {
+        return String.format("(%d,%d)", x, y);
     }
-    double distance(Point point){
-        return Math.sqrt(Math.pow(x-point.x,2)+Math.pow(y-point.y,2));
+
+    public double distance(Point point) {
+        if (point != null) {
+            return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+        } else throw new IllegalArgumentException("аргумент пуст");
     }
-    void flip(){
-        x=-x-y-(y=-x);
+
+    void flip() {
+        x = -x - y - (y = -x);
     }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
