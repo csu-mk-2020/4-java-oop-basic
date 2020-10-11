3137ee3b54c6dded678a98b6d58c925fbf320f29
gitlab_admin <admin@f-lab.tech>
2020-10-11 13:18:55 +0000

    Исправлены модификаторы доступа
    Добавлена проверка на NPE

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,5 +1,7 @@
 package com.example.task01;
 
+import java.util.Objects;
+
 /**
  * Класс точки на плоскости
  */
@@ -7,28 +9,29 @@ public class Point {
     int x;
     int y;
 
-    Point() {
+    public Point() {
         this.x = 0;
         this.y = 0;
     }
 
-    Point(int x, int y) {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    void print() {
+    public void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
 
-    void flip() {
+    public void flip() {
         int tmp = -this.x;
         this.x = -this.y;
         this.y = tmp;
     }
 
-    double distance(Point point) {
+    public double distance(Point point) throws NullPointerException {
+        Objects.requireNonNull(point, "Point must not be null");
         return Math.sqrt((Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2)));
     }
 
