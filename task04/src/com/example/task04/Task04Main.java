66d27911424c3fa71bcf44251e51771505ed5499
Jester <zavyalov.mikhail99@gmail.com>
2020-10-12 19:45:24 +0500

    task04

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,15 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line = new Line(new Point(0, 0), new Point(2, 2));
+        Point point = new Point(1, 1);
+        String message = "on a line containing a segment ";
+        if (line.isCollinearLine(point)){
+            message = " lies " + message;
+        }
+        else {
+            message = " does not lie " + message;
+        }
+        System.out.println(point.toString()+message+line.toString());
     }
 }
