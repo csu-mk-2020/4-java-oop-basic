e71de378379a2e1c02e8bc3ff2986c64a19f30e6
Petrov <qwe@qwe.com>
2020-10-13 21:44:15 +0500

    Исправил проверку на null

--- task04/src/com/example/task04/Line.java
+++ task04/src/com/example/task04/Line.java
@@ -1,12 +1,13 @@
 package com.example.task04;
 
+import java.util.Objects;
+
 public class Line {
     private final Point p1, p2;
 
     public Line(Point p1, Point p2) {
-        if (p1 == null || p2 == null) {
-            throw new NullPointerException("");
-        }
+        Objects.requireNonNull(p1);
+        Objects.requireNonNull(p2);
         this.p1 = p1;
         this.p2 = p2;
     }
@@ -24,9 +25,7 @@ public class Line {
     }
 
     public boolean isCollinearLine(Point p) {
-        if (p == null) {
-            throw new NullPointerException("");
-        }
+        Objects.requireNonNull(p);
         return (p.x * (p2.y - p1.y) - p.y * (p2.x - p1.x) == p1.x * p2.y - p2.x * p1.y);
     }
 }
