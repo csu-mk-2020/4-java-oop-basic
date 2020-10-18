52fef0c1e582c645bec3d86249779eedca0791a2
Jester <zavyalov.mikhail99@gmail.com>
2020-10-18 12:36:28 +0500

    task01 [Zavyalov]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,27 +1,39 @@
 package com.example.task01;
 
+import java.util.Objects;
+
 /**
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
-    Point(){
+    private int x;
+    private int y;
 
-    }
-
-    Point(int x, int y){
+    /**
+     * @param x координата x
+     * @param y координата y
+     */
+    public Point(int x, int y){
         this.x = x;
         this.y = y;
     }
 
-    void flip() {
+    /**
+     * Разворот точки на 180 (на самом деле нет)
+     */
+    public void flip() {
         int temp = -this.x;
         this.x = -this.y;
         this.y = temp;
     }
 
-    double distance(Point point){
+    /**
+     * @param point точка, до которой считается расстояние от текущей
+     * @return расстояние между точками
+     * @throws NullPointerException если point == null
+     */
+    public double distance(Point point) throws NullPointerException {
+        Objects.requireNonNull(point);
         return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
     }
 
@@ -29,7 +41,7 @@ public class Point {
         return String.format("(%d, %d)", x, y);
     }
 
-    void print() {
+    public void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
