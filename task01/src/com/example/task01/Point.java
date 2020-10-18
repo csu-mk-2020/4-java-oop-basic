5c9334c0e13b9799b33959925abec305a9f2955b
Виталий Вересников <Vitosiy21@yandex.ru>
2020-10-18 21:37:24 +0500

    Task01 [Veresnikov]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,13 +1,33 @@
 package com.example.task01;
-
+import java.util.Objects;
 /**
  * Класс точки на плоскости
  */
 public class Point {
-    int x;
-    int y;
+    private int x;
+    private int y;
 
-    void print() {
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public void flip() {
+        int temp = -this.x;
+        this.x = -this.y;
+        this.y = temp;
+    }
+
+    public double distance(Point point) throws NullPointerException {
+        Objects.requireNonNull(point);
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
+
+    public String toString(){
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
     }
--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -2,18 +2,23 @@ package com.example.task01;
 
 public class Task01Main {
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 45;
-        Point p2 = new Point();
-        p2.x = 78;
-        p2.y = 12;
+        Point p1 = new Point(30, 66);
+        Point p2 = new Point(21, 9);
+
+        double dis = p1.distance(p2);
 
         System.out.println("Point 1:");
         p1.print();
+        p1.flip();
+        System.out.println("Reverse point 1:");
         System.out.println(p1);
         System.out.println("Point 2:");
         p2.print();
+        p2.flip();
+        System.out.println("Reverse point 2:");
         System.out.println(p2);
+        System.out.println("Point1 - Point2 distance:");
+        System.out.println(dis);
+
     }
 }
