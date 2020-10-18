a7152bd1e5f5be31c96dcd77b64e9519d65397a0
Meghavor <picnik753@gmail.com>
2020-10-19 02:13:15 +0500

    task04[Никулин]

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,10 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
+        Line l = new Line(new Point(0,0), new Point(2, 2));
+        System.out.println(l);
+        System.out.println(l.isCollinearLine(new Point(1, 0)));
+        System.out.println(l.isCollinearLine(new Point(1, 1)));
 
     }
 }
