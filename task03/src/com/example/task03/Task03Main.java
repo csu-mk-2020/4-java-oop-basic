2e0a5ff71925d2cbcbb0fee55199417124c53038
lexa1329 <funin99@bk.ru>
2020-10-19 11:52:38 +0500

    task03[Фунин]

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,7 +1,22 @@
 package com.example.task03;
 
 public class Task03Main {
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
+            Complex a = new Complex(3, 4);
+            System.out.println(a.toString());
+            Complex b = new Complex(4, 5);
+            a = a.add(b);
+            System.out.println(a.toString());
+            a = a.sub(b);
+            System.out.println(a.toString());
+            a = a.mul(2);
+            System.out.println(a.toString());
+            a = a.mul(b);
+            System.out.println(a.toString());
+            a = a.div(b);
+            System.out.println(a.toString());
+            a = a.div(2);
+            System.out.println(a.toString());
+        }
     }
-}
