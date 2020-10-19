6a1b6ed6d320f036cc6671d485d23f70b6272cd7
lexa1329 <funin99@bk.ru>
2020-10-19 11:38:58 +0500

    task01[Фунин]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -1,5 +1,4 @@
 package com.example.task01;
-
 /**
  * Класс точки на плоскости
  */
@@ -7,8 +6,30 @@ public class Point {
     int x;
     int y;
 
+    Point(){
+    }
+
+    public Point(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
+    }
+
+    void flip() {
+        int tmp = -x;
+        x = -y;
+        y = tmp;
+    }
+
+    double distance(Point point) {
+        return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+    }
+
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
     }
 }
