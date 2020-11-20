2f8d4790419e066dd4fe03a464cf45f38366ab1a
Виталий Вересников <Vitosiy21@yandex.ru>
2020-11-20 21:40:13 +0500

    Task04 [Veresnikov]

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -2,6 +2,11 @@ package com.example.task04;
 
 public class Task04Main {
     public static void main(String[] args) {
-
+        Line line = new Line(new Point(0, 0), new Point(4, 4));
+        Point point = new Point(2, 2);
+        System.out.println(line.isCollinearLine(point));
+        System.out.println(line.getP1());
+        System.out.println(line.getP2());
+        System.out.println(line.toString());
     }
 }
