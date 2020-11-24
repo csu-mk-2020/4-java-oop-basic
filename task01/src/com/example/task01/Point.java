3a7ee52260697d4765baa27e2784e4f445389dd0
Astaroth <avm725@gmail.com>
2020-11-25 00:11:00 +0500

    Task01[Мысливко]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,6 +6,27 @@ package com.example.task01;
 public class Point {
     int x;
     int y;
+    public String toString() {
+        return String.format("(%d, %d)", x, y);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    void flip() {
+        int tmp = -this.x;
+        this.x = -this.y;
+        this.y = tmp;
+    }
+
+    double distance(Point point)  {
+        if (point == null) {
+            throw new IllegalArgumentException("point не может быть null");
+        }
+        return Math.sqrt(Math.pow(this.x - point.x, 2) + Math.pow(this.y - point.y, 2));
+    }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
