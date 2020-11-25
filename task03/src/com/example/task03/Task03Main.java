002257c8be4c3f400920134c62a3111d120e2dcd
Astaroth <avm725@gmail.com>
2020-11-25 08:52:58 +0500

    Task03[Мысливко]

--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -3,5 +3,9 @@ package com.example.task03;
 public class Task03Main {
     public static void main(String[] args) {
 
+        ComplexNumbers num1 = new ComplexNumbers(0, 1.1);
+        ComplexNumbers num2 = new ComplexNumbers(1.5, -2);
+        System.out.println(num1.add(num2).toString());
+        System.out.println(num1.mult(num2).toString());
     }
 }
