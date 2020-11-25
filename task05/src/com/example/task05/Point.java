4674f089f8a9e2d7276e44377e0d35eda19a71fb
Astaroth <avm725@gmail.com>
2020-11-25 09:40:42 +0500

    Task05[Мысливко]

--- task05/src/com/example/task05/Point.java
+++ task05/src/com/example/task05/Point.java
@@ -1,10 +1,12 @@
 package com.example.task05;
+import java.util.Objects;
 
 /**
  * Точка в двумерном пространстве
  */
 public class Point {
 
+    private double x, y;
     /**
      * Конструктор, инициализирующий координаты точки
      *
@@ -12,7 +14,17 @@ public class Point {
      * @param y координата по оси ординат
      */
     public Point(double x, double y) {
+        /*
         throw new AssertionError();
+        */
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point(Point point) {
+        Objects.requireNonNull(point);
+        this.x = point.x;
+        this.y = point.y;
     }
 
     /**
@@ -21,8 +33,11 @@ public class Point {
      * @return координату точки по оси X
      */
     public double getX() {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        return this.x;
     }
 
     /**
@@ -31,8 +46,11 @@ public class Point {
      * @return координату точки по оси Y
      */
     public double getY() {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        return this.y;
     }
 
     /**
@@ -41,8 +59,11 @@ public class Point {
      * @param x координата точки по оси X
      */
     public void setX(double x) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        this.x = x;
     }
 
     /**
@@ -51,8 +72,11 @@ public class Point {
      * @param y координата точки по оси Y
      */
     public void setY(double y) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        this.y = y;
     }
 
     /**
@@ -62,8 +86,12 @@ public class Point {
      * @return расстояние от текущей точки до переданной
      */
     public double getLength(Point point) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        Objects.requireNonNull(point);
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
     }
 
 }
--- task05/src/com/example/task05/PolygonalLine.java
+++ task05/src/com/example/task05/PolygonalLine.java
@@ -1,17 +1,28 @@
 package com.example.task05;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Ломаная линия
  */
 public class PolygonalLine {
+    private final ArrayList<Point> points = new ArrayList<>();
 
     /**
      * Устанавливает точки ломаной линии
      *
      * @param points массив точек, которыми нужно проинициализировать ломаную линию
      */
-    public void setPoints(Point[] points) {
+    public void setPoints(Point[] points) throws NullPointerException{
         // TODO: реализовать
+        Objects.requireNonNull(points);
+        for (Point point: points) {
+            Objects.requireNonNull(point);
+        }
+        this.points.clear();
+        for (Point point : points) {
+            this.points.add(new Point(point));
+        }
     }
 
     /**
@@ -21,6 +32,8 @@ public class PolygonalLine {
      */
     public void addPoint(Point point) {
         // TODO: реализовать
+        Objects.requireNonNull(point);
+        this.points.add(new Point(point));
     }
 
     /**
@@ -31,6 +44,7 @@ public class PolygonalLine {
      */
     public void addPoint(double x, double y) {
         // TODO: реализовать
+        this.points.add(new Point(x, y));
     }
 
     /**
@@ -39,8 +53,15 @@ public class PolygonalLine {
      * @return длину ломаной линии
      */
     public double getLength() {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        double length = 0d;
+        for (int i = 0; i < this.points.size() - 1; i++) {
+            length += this.points.get(i).getLength(this.points.get(i + 1));
+        }
+        return length;
     }
 
 }
