c6101cd1901d1a5847673d4281671f55ec75d56d
Astaroth <avm725@gmail.com>
2020-11-25 10:20:27 +0500

    task05[Мысливко]

--- task05/src/com/example/task05/Point.java
+++ task05/src/com/example/task05/Point.java
@@ -1,9 +1,11 @@
 package com.example.task05;
+import java.util.Objects;
 
 /**
  * Точка в двумерном пространстве
  */
 public class Point {
+    private double x, y;
 
     /**
      * Конструктор, инициализирующий координаты точки
@@ -12,17 +14,29 @@ public class Point {
      * @param y координата по оси ординат
      */
     public Point(double x, double y) {
+        /*
         throw new AssertionError();
+        */
+        this.x = x;
+        this.y = y;
+    }
+    public Point(Point point) {
+        Objects.requireNonNull(point);
+        this.x = point.x;
+        this.y = point.y;
     }
 
-    /**
-     * Возвращает координату точки по оси абсцисс
-     *
-     * @return координату точки по оси X
-     */
+        /**
+         * Возвращает координату точки по оси абсцисс
+         *
+         * @return координату точки по оси X
+         */
     public double getX() {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        return this.x;
     }
 
     /**
@@ -31,8 +45,11 @@ public class Point {
      * @return координату точки по оси Y
      */
     public double getY() {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        return this.y;
     }
 
     /**
@@ -41,8 +58,11 @@ public class Point {
      * @param x координата точки по оси X
      */
     public void setX(double x) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        this.x = x;
     }
 
     /**
@@ -51,8 +71,11 @@ public class Point {
      * @param y координата точки по оси Y
      */
     public void setY(double y) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        this.y = y;
     }
 
     /**
@@ -62,8 +85,12 @@ public class Point {
      * @return расстояние от текущей точки до переданной
      */
     public double getLength(Point point) {
+        /*
         // TODO: реализовать
         throw new AssertionError();
+        */
+        Objects.requireNonNull(point);
+        return Math.sqrt(Math.pow(point.x - this.x, 2) + Math.pow(point.y - this.y, 2));
     }
 
 }
