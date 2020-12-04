4be9285cd83b1b7d6e5536584d727b19514a92c3
lexa1329 <funin99@bk.ru>
2020-12-04 13:26:33 +0500

    task01[Funin]

--- task01/src/com/example/task01/Point.java
+++ task01/src/com/example/task01/Point.java
@@ -6,30 +6,27 @@ public class Point {
     int x;
     int y;
 
-    Point(){
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public Point(int x, int y){
-        this.x=x;
-        this.y=y;
+    public String toString() {
+        return String.format("(%d,%d)", x, y);
+    }
+
+    public double distance(Point point) {
+        if (point != null) {
+            return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
+        } else throw new IllegalArgumentException("аргумент пуст");
+    }
+
+    void flip() {
+        x = -x - y - (y = -x);
     }
 
     void print() {
         String pointToString = String.format("(%d, %d)", x, y);
         System.out.println(pointToString);
-    }
-
-    void flip() {
-        int tmp = -x;
-        x = -y;
-        y = tmp;
-    }
-
-    double distance(Point point) {
-        return Math.sqrt(Math.pow(x - point.x, 2) + Math.pow(y - point.y, 2));
-    }
-
-    public String toString() {
-        return String.format("(%d, %d)", x, y);
     }
 }
