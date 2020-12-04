841c70fba0bb8c2f8f2afebca2c43b2d9eccfe99
lexa1329 <funin99@bk.ru>
2020-12-04 13:32:06 +0500

    task03[Funin]

--- task03/src/com/example/task03/Complex.java
+++ task03/src/com/example/task03/Complex.java
@@ -1,80 +1,46 @@
 package com.example.task03;
 
+/**
+ * Класс комплексных чисел
+ */
 public class Complex {
-    private long x;
-    private long y;
+    private double re;
+    private double im;
 
-    Complex() {
-        x = 0;
-        y = 0;
+    public Complex(double re, double im) {
+        this.re = re;
+        this.im = im;
     }
 
-    Complex(long x, long y) {
-        this.x = x;
-        this.y = y;
+    public double getRe() {
+        return re;
     }
 
-    Complex(Complex complex) {
-        if(complex==null){
-            throw new IllegalArgumentException();
+    public double getIm() {
+        return im;
+    }
+
+    public static Complex sum(Complex val1, Complex val2) {
+        if (val1 == null) {
+            throw new IllegalArgumentException("первый аргумент пуст");
         }
-        this.x = complex.x;
-        this.y = complex.y;
-    }
-
-    Complex add(Complex complex) {
-        if(complex==null){
-            throw new IllegalArgumentException();
+        if (val2 == null){
+            throw new IllegalArgumentException("второй аргумент пуст");
         }
-        return new Complex(x + complex.x, y + complex.y);
+        return new Complex(val2.getRe() + val1.getRe(), val2.getIm() + val1.getIm());
     }
 
-    Complex sub(Complex complex) {
-        if(complex==null){
-            throw new IllegalArgumentException();
+    public static Complex multiply(Complex val1, Complex val2) {
+        if (val1 == null) {
+            throw new IllegalArgumentException("первый аргумент пуст");
         }
-        return new Complex(x - complex.x, y - complex.y);
-    }
-
-    Complex mul(Complex complex) {
-        if(complex==null){
-            throw new IllegalArgumentException();
+        if (val2 == null){
+            throw new IllegalArgumentException("второй аргумент пуст");
         }
-        return new Complex(x * complex.x - y * complex.y, y * complex.x + x * complex.y);
-    }
-
-    Complex mul(long a) {
-        return new Complex(x * a, y * a);
-    }
-
-    Complex div(Complex complex) {
-        if(complex==null){
-            throw new IllegalArgumentException();
-        }
-        return new Complex((x * complex.x + y * complex.y) / (complex.x * complex.x + complex.y * complex.y), (y * complex.x - x * complex.y) / (complex.x * complex.x + complex.y * complex.y));
-    }
-
-    Complex div(long a) {
-        return new Complex(x / a, y / a);
+        return new Complex(val1.getRe() * val2.getRe() - val1.getIm() * val2.getIm(), val1.getRe() * val2.getIm() + val1.getIm() * val2.getRe());
     }
 
     public String toString() {
-        return y > 0 ? String.format("%d+%di", x, y) : String.format("%d%di", x, y);
-    }
-
-    long getX() {
-        return x;
-    }
-
-    long getY() {
-        return y;
-    }
-
-    public void setX(long x) {
-        this.x = x;
-    }
-
-    public void setY(long y) {
-        this.y = y;
+        return String.format("%.2f+%.2fi", re, im);
     }
 }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -2,21 +2,10 @@ package com.example.task03;
 
 public class Task03Main {
         public static void main(String[] args) {
+            Complex src = new Complex(1,2);
+            Complex dst = new Complex(3,4);
 
-            Complex a = new Complex(3, 4);
-            System.out.println(a.toString());
-            Complex b = new Complex(4, 5);
-            a = a.add(b);
-            System.out.println(a.toString());
-            a = a.sub(b);
-            System.out.println(a.toString());
-            a = a.mul(2);
-            System.out.println(a.toString());
-            a = a.mul(b);
-            System.out.println(a.toString());
-            a = a.div(b);
-            System.out.println(a.toString());
-            a = a.div(2);
-            System.out.println(a.toString());
+            System.out.println(Complex.sum(src,dst).toString());
+            System.out.println(Complex.multiply(src,dst).toString());
         }
     }
